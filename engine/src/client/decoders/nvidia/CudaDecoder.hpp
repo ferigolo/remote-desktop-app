91adbaf6c5197003d890daea117d33b0208abc16
Enzo Ferigolo <ferigolochielleenzo@gmail.com>
2026-07-08 09:27:21 -0300

    Passed responsability to Renderer. Created Cuda decoder

--- engine/src/client/decoders/nvidia/CudaDecoder.hpp
+++ engine/src/client/decoders/nvidia/CudaDecoder.hpp
@@ -4,7 +4,15 @@ class CudaDecoder : public BaseDecoder {
  public:
   CudaDecoder();
   ~CudaDecoder();
+  bool initialize() override;
+
+  void decode(const uint8_t* data, size_t size) override;
 
  private:
-  /* data */
+  AVPacket* packet{};
+  AVFrame* frame{};
+  AVBufferRef* hwDeviceCtx{};
+
+  static AVPixelFormat getHwFormat(AVCodecContext* ctx,
+                                   const AVPixelFormat* pixFmts);
 };
